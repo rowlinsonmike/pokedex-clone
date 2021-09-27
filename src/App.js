fedf769e0ae4370edb90b1cd458f60763988b368
Michael Rowlinson <michael.rowlinson@westrock.com>
2021-09-27 17:22:56 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Browser} />
+        <Route exact path="/pokemon/:pokemon" component={Pokemon} />
+        <Route path="*" component={FourOhFour} />
+      </Switch>
     </div>
   );
 }
 
-export default App;
+function Browser() {
+  return (
+    <div>
+      <h2>Browser</h2>
+    </div>
+  );
+}
+
+function Pokemon() {
+  return (
+    <div>
+      <h2>Pokemon</h2>
+    </div>
+  );
+}
+
+function FourOhFour() {
+  return (
+    <div>
+      <h2>404</h2>
+    </div>
+  );
+}
---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import "normalize.css";
+
+export default createGlobalStyle`
+:root{
+   
+}
+*,*:after,*:before{
+    box-sizing: border-box;
+}
+html{
+    
+}
+body{
+    
+}
+`;
